4d4d93c7293d70838fd159f69600e6526d9c22cc
sveet <sveetlana2000@mail.ru>
2022-09-20 23:24:49 +0300

    Java Core level 2 tasks 1206-1210

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(8);
+        Integer integer = 8;
+        print(integer);
+    }
+
+    static void print(int i){
+
+    }
+
+    static void print(Integer integer){}
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    static void print(int i){}
+
+    static void print(Integer i){}
+
+    static void print(String s){}
+
+    static void print(double d){}
+
+    static void print(Character c){}
+}
