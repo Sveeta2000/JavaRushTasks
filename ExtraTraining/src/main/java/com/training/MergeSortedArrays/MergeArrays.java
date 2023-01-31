45434bd0e18b7e2ad715bb81dd9804d2f91e4147
sveet <sveetlana2000@mail.ru>
2023-01-31 21:05:09 +0300

    Extra training tasks

---
+++ ExtraTraining/src/main/java/com/training/MergeSortedArrays/MergeArrays.java
@@ -0,0 +1,26 @@
+package com.training.MergeSortedArrays;
+
+import java.util.Arrays;
+
+/**
+ * @author sveet
+ * @date 31.01.2023
+ */
+public class MergeArrays {
+
+    public static void main(String[] args) {
+        int[] array1 = {1,2,3,0,0,0};
+        int[] array2 = {2,5,6};
+        merge(array1, 3, array2, 3);
+        System.out.println(Arrays.toString(array1));
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index2 = 0;
+        for (int i = m; i < nums1.length; i++) {
+            nums1[i] = nums2[index2];
+            index2++;
+        }
+        Arrays.sort(nums1);
+    }
+}
---
+++ ExtraTraining/src/main/java/com/training/RemoveDuplicatesFromSortedArray/RemoveDuplicates.java
@@ -0,0 +1,37 @@
+package com.training.RemoveDuplicatesFromSortedArray;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/* Constraints:
+1 <= nums.length <= 3 * 104
+-100 <= nums[i] <= 100
+nums is sorted in non-decreasing order.
+ */
+
+/**
+ * @author sveet
+ * @date 31.01.2023
+ */
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        int[] array = {1, 1, 1, 2, 4};
+        System.out.println(removeDuplicates(array));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        Set<Integer> originalNumbers = new TreeSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!originalNumbers.contains(nums[i])){
+                originalNumbers.add(nums[i]);
+            }
+        }
+        int i = 0;
+        for (int number : originalNumbers) {
+            nums[i] = number;
+            i++;
+        }
+        return originalNumbers.size();
+    }
+}
